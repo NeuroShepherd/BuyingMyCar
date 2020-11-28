93466ed1754e2f1cbaa85104c64da47857a9d133
Patrick Callahan <patricktcallahan18@gmail.com>
2020-11-28 17:37:06 -0600

    Update url_generator_fxn.R to allow removal of online dealers from results; add {shiny} to packages and notes.R

--- url_generator_fxn.R
+++ url_generator_fxn.R
@@ -1,22 +1,40 @@
 
 
 
-truecar_url_creator <- function(make,model=NA,location,min_year,max_year="max",searchRadius=75){
+truecar_url_creator <- function(make,model=NA,location,min_year,max_year="max",
+                                searchRadius=75,online_dealers=FALSE){
   
   model <- ifelse(is.na(model),"/",paste0("/",model,"/"))
   
-  pagination <- glue::glue('https://www.truecar.com/used-cars-for-sale/listings/{make}{model}year-{min_year}-{max_year}/location-{location}/?page=1&searchRadius={searchRadius}') %>%
-    read_html() %>%
-    html_node(css = ".d-flex.justify-content-center") %>%
-    html_text() %>%
-    stringr::str_extract(.,"(\\d+)$") 
+  # Ifelse for filtering out online dealers when creating slugs
+  pagination <- if (online_dealers) {
+    glue::glue('https://www.truecar.com/used-cars-for-sale/listings/{make}{model}year-{min_year}-{max_year}/location-{location}/?page=1&searchRadius={searchRadius}') %>%
+      read_html() %>%
+      html_node(css = ".d-flex.justify-content-center") %>%
+      html_text() %>%
+      stringr::str_extract(.,"(\\d+)$") 
+  } else {
+    glue::glue('https://www.truecar.com/used-cars-for-sale/listings/{make}{model}year-{min_year}-{max_year}/location-{location}/?onlineDealers=none&page=1&searchRadius={searchRadius}') %>%
+      read_html() %>%
+      html_node(css = ".d-flex.justify-content-center") %>%
+      html_text() %>%
+      stringr::str_extract(.,"(\\d+)$")
+  }
+   
+  
   
   # Some make/model combos only have 1 page so the pagination number is just listed as chr ""
   # Create ifelse statement to set "" to 1
   npages<- ifelse(is.na(pagination), 1, pagination) %>% seq_len()
 
-   glue::glue('https://www.truecar.com/used-cars-for-sale/listings/{make}{model}year-{min_year}-max/location-{location}/?page={npages}&searchRadius={searchRadius}') %>%
-    return()
+  # Return links including or w/o onlineDealers slug
+  if (online_dealers){
+    glue::glue('https://www.truecar.com/used-cars-for-sale/listings/{make}{model}year-{min_year}-max/location-{location}/?page={npages}&searchRadius={searchRadius}') %>%
+      return()
+  } else {
+    glue::glue('https://www.truecar.com/used-cars-for-sale/listings/{make}{model}year-{min_year}-max/location-{location}/?onlineDealers=none&page={npages}&searchRadius={searchRadius}') %>%
+      return()
+  }
 }
 
 
